10bf26fe556ae2ceac392793689ea8965c17182d
Gerardo Bautista <gerardoemery@live.com.mx>
2022-05-06 23:30:06 -0500

    subject

--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -1,17 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable} from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 import { User } from './user.model';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   url = 'https://desa.ies-webcontent.com.mx'
-  constructor(private http: HttpClient) {
+  private authSubject = new Subject<boolean>()
+  private isLoggedIn = false
+  constructor(private http: HttpClient, private router: Router) {}
 
+  getIsLogged() {
+    return this.isLoggedIn
   }
-  login (user: User): Observable<User> {
+
+  getAuthListen() {
+    return this.authSubject.asObservable()
+  }
+
+  login (user: User){
     return this.http.post<User>(this.url+'/login',user)
+    .subscribe({
+      next: c => {
+        this.authSubject.next(true)
+        this.router.navigate(['/welcome'])
+      },
+      error: error => {
+        this.authSubject.next(false)
+      },
+      complete: () => {
+        console.log('Request complete');
+      }
+    });
   }
 
 
--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../auth.service';
 import { User } from '../user.model';
 
@@ -12,6 +13,8 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
   user!: User;
   loading = false;
+  authSubscription: Subscription
+
   constructor(
     private formBuilder: FormBuilder,
     private auth: AuthService
@@ -20,9 +23,16 @@ export class LoginComponent implements OnInit {
       username: ['',Validators.required],
       password: ['',Validators.required],
     })
+
+    this.authSubscription = this.auth.getAuthListen()
+    .subscribe(authL => {
+      this.loading = authL
+    })
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
 
   get f() { return this.form.controls }
 
@@ -31,12 +41,10 @@ export class LoginComponent implements OnInit {
     this.user = {
       username: this.f['username'].value,password:this.f['password'].value
     }
-    this.auth.login(this.user).subscribe(res => {
-      console.log(res)
-    },error => {
-      this.loading = false
+    this.auth.login(this.user)
+    setTimeout(() => {
       this.form.reset()
-    })
+    }, 1000);
   }
 
 }
