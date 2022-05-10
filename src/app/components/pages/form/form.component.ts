13ce6c520193b5695e966411d19e10113237fe3e
Gerardo Bautista <gerardoemery@live.com.mx>
2022-05-10 13:27:26 -0500

    form controls

--- src/app/components/pages/form/form.component.ts
+++ src/app/components/pages/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form: FormGroup
+  formBooks!:FormArray
+  disabled = true
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      nombres: ['',Validators.required],
+      apellidos: ['',Validators.required],
+      fumas: ['',Validators.required],
+      actualmentePracticasLectura: [true,Validators.required],
+      librosLeidosUltimosTresMeses: this.formBuilder.array([this.setBooksArray()]),
+      estadoCivil: ['']
+    })
   }
 
+  ngOnInit(): void {
+    this.checkLecture()
+  }
+
+  generateUniqueId() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
+
+  checkLecture() {
+    this.form.controls['actualmentePracticasLectura'].valueChanges.subscribe(check => {
+      if(check) {
+        this.disabled = true
+        this.form.addControl('librosLeidosUltimosTresMeses', new FormArray([this.setBooksArray()]))
+      } else {
+        this.disabled = false
+        this.form.removeControl('librosLeidosUltimosTresMeses')
+      }
+    });
+  }
+
+  addBook() {
+    this.fa.push(this.setBooksArray());
+  }
+
+  setBooksArray() {
+    return this.formBuilder.group({
+      book: this.formBuilder.control('Dorian Grey', Validators.required),
+      id: this.generateUniqueId()
+    });
+  }
+
+
+
+  get fa() { return this.form.get('librosLeidosUltimosTresMeses') as FormArray; }
+
+  send() {
+    console.log(this.form.value)
+  }
 }
